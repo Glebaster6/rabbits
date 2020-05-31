1dfbc84336cbe18cc92befffb6ad9a63e5bf63cb
glebaster <glebaster6@yandex.ru>
2020-05-31 14:56:30 +0300

    master
       + agregator fix

--- Agregator/src/main/java/ae/agregator/messgaing/listeners/MainProducerListener.java
+++ Agregator/src/main/java/ae/agregator/messgaing/listeners/MainProducerListener.java
@@ -1,5 +1,6 @@
 package ae.agregator.messgaing.listeners;
 
+import lombok.SneakyThrows;
 import main.dto.MainDto;
 import main.dto.MainUtil;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,15 +14,18 @@ public class MainProducerListener {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @SneakyThrows
     @RabbitListener(queues = "mainProducerToAgregatorQueue")
     public void listen(byte[] in) {
         MainDto mainDto = (MainDto) MainUtil.byteArrayToObject(in);
+        System.out.println(mainDto.getAction());
         switch (mainDto.getAction()){
             case LOGIN:
             case REGISTER:
             case GET_FACILITY_DATA:
             case DELETE_EVALUATION:
             case GET_EVALUATION_DATA:
+            case TRAIN_MODEL:
             case GET_EVALUATION_DATA_BY_EVALUATION_AND_PERIOD:
             case PARSE_EXCEL:{
                 rabbitTemplate.convertAndSend("agregatorToUpdaterQueue", MainUtil.objectToByteArray(mainDto));
@@ -29,6 +33,13 @@ public class MainProducerListener {
             }
             case GET_COMMODITY_GROUP_RESULT:{
                 rabbitTemplate.convertAndSend("agregatorToMarketAnalysisQueue", MainUtil.objectToByteArray(mainDto));
+                Thread.sleep(200);
+                mainDto.setAction(MainDto.Action.GET_PREDICTION);
+                rabbitTemplate.convertAndSend("agregatorToNeuralNetAnalysisQueue", MainUtil.objectToByteArray(mainDto));
+                break;
+            }
+            case GET_PREDICTION:{
+                rabbitTemplate.convertAndSend("agregatorToNeuralNetAnalysisQueue", MainUtil.objectToByteArray(mainDto));
                 break;
             }
         }
--- Agregator/src/main/java/ae/agregator/messgaing/listeners/NeuralNetAnalysisListener.java
+++ Agregator/src/main/java/ae/agregator/messgaing/listeners/NeuralNetAnalysisListener.java
@@ -1,12 +1,25 @@
 package ae.agregator.messgaing.listeners;
 
+import main.dto.MainDto;
+import main.dto.MainUtil;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NeuralNetAnalysisListener {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @RabbitListener(queues = "neuralNetAnalysisToAgregatorQueue")
-    public void listen(String in) {
-        System.out.println("Message read from myQueue : " + in);
+    public void listen(byte[] in) {
+        MainDto mainDto = (MainDto) MainUtil.byteArrayToObject(in);
+        switch (mainDto.getAction()){
+            case RETURN_PREDICTION:{
+                rabbitTemplate.convertAndSend("agregatorToMainProducerQueue", MainUtil.objectToByteArray(mainDto));
+                break;
+            }
+        }
     }
 }
